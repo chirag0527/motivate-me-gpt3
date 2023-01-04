affa1f3f4b96cbe1c37476cd42d71f94f3aacbd2
chirag0527 <chiragpaul1989@Gmail.com>
2023-01-04 17:27:27 +0530

    wrote the logic and functions

---
+++ pages/api/generate2.js
@@ -0,0 +1,34 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const basePromptPrefix = `make sure to add "🤬" emoji at every sentence 
+
+motivate me to read a new book
+Get off your bed you lazy piece of shit. Stop scrolling through Instagram and go out to the library🤬
+
+motivate me to go to the gym
+This is why you don't get any girls. Stop watching Netflix and go to the fucking gym🤬
+
+motivate me to`;
+const generateAction = async (req, res) => {
+  // Run first prompt
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.2,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,70 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [isChecked1, setIsChecked1] = useState(null)
+  const [isChecked2, setIsChecked2] = useState(null)
+
+  function handleChange1(e) {
+    setIsChecked1(e.target.value === 'on' ? true : false);
+    setIsChecked2(!isChecked1)
+  }
+
+  function handleChange2(e) {
+    setIsChecked2(e.target.value === 'on' ? true : false);
+    setIsChecked1(!isChecked2)
+  }
+
+  const callGenerateEndpoint1 = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  const callGenerateEndpoint2 = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate2', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+
+  function onUserChangedText(e){
+    setUserInput(e.target.value)
+  }
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +73,49 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>get <span>motivated</span></h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Not feeling motivated today? Just write the task you wanna work on.</h2>
           </div>
         </div>
+        <div className='prompt-container'>
+          <textarea placeholder='read a new book' 
+          className='prompt-box' 
+          value = {userInput}
+          onChange = {onUserChangedText}
+          />
+        </div>
+        <form className='form'>
+            <label className="checkbox-container" onChange={handleChange1}>🙂
+                <input type="radio" name="choice"></input>
+            </label>
+            <label className="checkbox-container" onChange={handleChange2}>😠
+                <input type="radio" name="choice"></input>
+            </label>
+        </form>
+        <div className="prompt-buttons">
+          <a
+            className={isGenerating ? 'generate-button loading' : 'generate-button'}
+            onClick={isChecked1?callGenerateEndpoint1:callGenerateEndpoint2}
+          >
+            <div className="generate">
+            {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+            </div>
+          </a>
+        </div>
+        {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
@@ -26,7 +125,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built with buildspace</p>
           </div>
         </a>
       </div>
